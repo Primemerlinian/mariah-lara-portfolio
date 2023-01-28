58f0e46384f8781861bb97173b6f3367790fbb77
Primemerlinian <mwatson0518@gmail.com>
2023-01-27 23:19:20 -0500

    add import pages to app.js

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function NavBar () {
+  return (
+    <header class="App-Header">
+  <Link to="/">Home</Link>
+  {/* <Link to="/Projects">Projects</Link>
+  <Link to="/Resume">Resume</Link>
+  <Link to="/Contact">Contact</Link>
+  <Link to="/AboutMe">About Me</Link> */}
+  </header>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/About'
+import About from './pages/AboutMe'
 import Contact from './pages/Contact'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
