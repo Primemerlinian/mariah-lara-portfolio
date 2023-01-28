57c48ac16c5bd1ef68ea82803b6c0c27f4dff72c
Primemerlinian <mwatson0518@gmail.com>
2023-01-27 23:24:51 -0500

    add route path for home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+// import Contact from './pages/Contact'
+// import Resume from './pages/Resume'
+// import Projects from './pages/Projects'
+// import AboutMe from './pages/AboutMe'
+import NavBar from './components/NavBar'
 
-import NavBar from './components/NavBar';
 function App() {
 
   return (
     <>
-
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
 
     </>
   );
