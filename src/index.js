777c2f7acb626f355cc3acf50142b99807441ab0
Primemerlinian <mwatson0518@gmail.com>
2023-01-27 23:34:59 -0500

    stub up contact function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import Home from './pages/Home'
 import About from './pages/AboutMe'
 import Contact from './pages/Contact'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = 
+ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
@@ -15,4 +18,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
