b653bb7c315ddbc7a77e5d749548ce201c1238a8
Primemerlinian <mwatson0518@gmail.com>
2023-01-28 00:05:55 -0500

    add function for pages/home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
-// import Home from './pages/Home'
+import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Resume from './pages/Resume'
 import Projects from './pages/Projects'
@@ -13,7 +13,7 @@ function App() {
     <>
     <NavBar />
     <Routes>
-    {/* <Route path='/' element={<Home />} /> */}
+    <Route path='/' element={<Home />} />
     <Route path='/contact' element={<Contact />} />
     <Route path='/resume' element={<Resume />} />
     <Route path='/aboutme' element={<AboutMe />} />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function NavBar () {
   return (
-    <header class="App-Header">
+    <header>
   <Link to="/">Home</Link>
   <Link to="/Projects">Projects</Link>
   <Link to="/Resume">Resume</Link>
