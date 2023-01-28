48da83d553b1b7ee2926a6a1c2d6239e97205d2a
Primemerlinian <mwatson0518@gmail.com>
2023-01-28 17:29:47 -0500

    add contact info

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 function Contact() {
 
   return (
-
-      <h1>Contact</h1>
-
+    <>
+      <h1>Contact Me!</h1>
+      <p>mwatson0518@gmail.com</p>
+      <p>github.com/Primemerlinian</p>
+    </>
   )
 
 }
