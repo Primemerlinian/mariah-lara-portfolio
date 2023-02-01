1b6ad2e4ef21252cea87243f3fd58dc52d29456b
Primemerlinian <mwatson0518@gmail.com>
2023-01-31 19:41:15 -0500

    add projects to data file

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+export let projects = [
+  {
+    title: "AskMe Coffee",
+    description: "Input your favorite Coffee!",
+    image: "https://i.imgur.com/FUJjW7J.png",
+    repositoryLink: "https://github.com/Primemerlinian/AskMeCoffee.git",
+    deploymentLink: "https://askmecoffee.fly.dev/",
+    tech: "HTML5, CSS3, JavaScript, Express.js, Mongoose, MongoDB, EJS, Google OAuth."
+  },
+  {
+    title: "Beauty",
+    description: "Your new favorite online pet!",
+    image: "https://i.imgur.com/SUzNdbn.png",
+    repositoryLink: "https://github.com/Primemerlinian/Beauty",
+    deploymentLink: "https://beauty-simulator.netlify.app/",
+    tech: "HTML5, CSS3, JavaScript"
+  },
+  {
+    title: "Connect4",
+    description: "Online Connect4 game! ",
+    image: "https://i.imgur.com/SUoEKYn.png",
+    repositoryLink: "https://github.com/Primemerlinian/Connect4",
+    deploymentLink: "https://mariah-lara-connect-4.netlify.app/",
+    tech: "HTML5, CSS3, JavaScript"
+  },
+  {
+    title: "Pixel-Toe",
+    description: "Online pixelated Tic-Tac-Toe game!",
+    image: "https://i.imgur.com/IGo3PGL.png",
+    repositoryLink: "https://github.com/Primemerlinian/ttt-weekend.git",
+    deploymentLink: "https://pixel-toe-mariah-lara.netlify.app/",
+    tech: "HTML5, CSS3, JavaScript"
+  }
+
+
+]
