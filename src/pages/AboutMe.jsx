535bd89c6be64d947eaa6e0c736f0d0628b3566a
Primemerlinian <mwatson0518@gmail.com>
2023-02-01 23:52:06 -0500

    complete about me section of portfolio

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,11 +4,9 @@ function AboutMe() {
       <> 
       <h1>About Me!</h1>
       <img src="https://i.imgur.com/vYUd0Z2.jpg" className="castlephoto" alt="myphoto"/>
-      <h2>I have a love for Disney, spending quality time with my beloved pets (Beauty and Chicken), I also enjoy coding and spending time with family.
-
-
-
+      <h2>In addition to being a proud pet parent of both a dog (Beauty) and  a cat (Chicken), I also love to make the most of my free time by visiting DisneyWorld, spending quality time with my family over delicious tacos, and creating cherished memories through fun activities.
 </h2>
+<h2>I am a big fan of exploring flea markets as they offer an element of surprise and the potential to discover hidden treasures. In the same vein, I also relish the opportunity to fully immerse myself in coding, taking the time to thoroughly understand each language and deepen my knowledge.</h2>
       </>
 
   )
