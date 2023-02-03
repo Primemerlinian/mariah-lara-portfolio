279643bd8b5ae4e0512e7650dace554891a8188c
Primemerlinian <mwatson0518@gmail.com>
2023-02-03 18:26:55 -0500

    fix main photo for landing page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,6 +7,7 @@ function AboutMe() {
       <h2>In addition to being a proud pet parent of both a dog (Beauty) and  a cat (Chicken), I also love to make the most of my free time by visiting DisneyWorld, spending quality time with my family over delicious tacos, and creating cherished memories through fun activities.
 </h2>
 <h2>I am a big fan of exploring flea markets as they offer an element of surprise and the potential to discover hidden treasures. In the same vein, I also relish the opportunity to fully immerse myself in coding, taking the time to thoroughly understand each language and deepen my knowledge.</h2>
+<h2>I am proud to be a Latina and a member of the Latinas in Tech Miami chapter, where I celebrate and embrace my heritage and culture.</h2>
       </>
 
   )
