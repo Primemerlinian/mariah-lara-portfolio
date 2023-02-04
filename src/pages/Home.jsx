04aaa5e73d234f495834cb695ec3a1ef263c746e
Primemerlinian <mwatson0518@gmail.com>
2023-02-03 23:24:42 -0500

    add home paragraph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
 
       <h2> A full-stack Software Engineer with an insatiable curiosity that fuels my ambition to learn more. I have extensive experience in developing dynamic and interactive web applications using technologies such as HTML, CSS, JavaScript, React, Mongoose, MongoDB, Node, and Google OAuth.</h2>
       <h2>I take great pleasure in building and developing software, even if the end result may not be perfect. The process of learning and creating is what I find most enjoyable and satisfying.</h2>
+      <h2>I am proud to be a Latina and a member of the Latinas in Tech Miami chapter, where I celebrate and embrace my heritage and culture.</h2>
       </>
   )
 }
