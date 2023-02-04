395a6cf999899f15fa6d1c42d70af376534d203d
Primemerlinian <mwatson0518@gmail.com>
2023-02-03 23:22:41 -0500

    fix photos on mobile mode

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,7 +3,7 @@ function AboutMe() {
   return (
       <> 
       <h1>About Me!</h1>
-      <img src="https://i.imgur.com/vYUd0Z2.jpg" className="castlephoto" alt="myphoto"/>
+      <img src="https://i.imgur.com/vYUd0Z2.jpg" width="375" height="600" className="castlephoto" alt="myphoto"/>
       <h2>In addition to being a proud pet parent of both a dog (Beauty) and  a cat (Chicken), I also love to make the most of my free time by visiting DisneyWorld, spending quality time with my family over delicious tacos, and creating cherished memories through fun activities.
 </h2>
 <h2>I am a big fan of exploring flea markets as they offer an element of surprise and the potential to discover hidden treasures. In the same vein, I also relish the opportunity to fully immerse myself in coding, taking the time to thoroughly understand each language and deepen my knowledge.</h2>
